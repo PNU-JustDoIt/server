6816cdd05414543bb4d262400c295f08d342d037
harrysci <59914405+harrysci@users.noreply.github.com>
2021-11-12 17:15:29 +0900

    feat : lecture 모듈 생성
    1. controller , service, module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LectureController } from './lecture/lecture.controller';
+import { LectureModule } from './lecture/lecture.module';
+import { LectureService } from './lecture/lecture.service';
+import { LectureController } from './lecture/lecture.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LectureModule],
+  controllers: [AppController, LectureController],
+  providers: [AppService, LectureService],
 })
 export class AppModule {}
